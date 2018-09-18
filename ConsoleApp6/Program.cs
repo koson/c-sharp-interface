90a73c78aa2ca24157b3c7aaef89f8916c854d20
koson trachu <koson.trachu@gmail.com>
2018-09-19 05:54:36 +0700

    References to Multiple Interfaces

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -9,19 +9,17 @@ namespace ConsoleApp6
     class Program
     {
 
-        interface IIfc1
+        interface IIfc1 // Declare interface.
         {
             void PrintOut(string s);
         }
-        interface IIfc2
+        interface IIfc2 // Declare interface
         {
-            void PrintOut(string t);
+            void PrintOut(string s);
         }
-
-
-        class MyClass : IIfc1, IIfc2 // Implement both interfaces.
+        class MyClass : IIfc1, IIfc2 // Declare class.
         {
-            public void PrintOut(string s) // Single implementation for both
+            public void PrintOut(string s)
             {
                 Console.WriteLine("Calling through: {0}", s);
             }
@@ -31,8 +29,12 @@ namespace ConsoleApp6
         static void Main()
         {
             MyClass mc = new MyClass();
-            mc.PrintOut("object");
+            IIfc1 ifc1 = (IIfc1)mc; // Get ref to IIfc1.
+            IIfc2 ifc2 = (IIfc2)mc; // Get ref to IIfc2.
 
+            mc.PrintOut("object"); // Call through class object.
+            ifc1.PrintOut("interface 1"); // Call through IIfc1.
+            ifc2.PrintOut("interface 2"); // Call through IIfc2.
         }
 
     }
