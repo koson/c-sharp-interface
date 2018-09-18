17789f76c569395d1993319111a7780bec959775
koson trachu <koson.trachu@gmail.com>
2018-09-19 03:05:50 +0700

    add example 2

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -6,44 +6,25 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp6
 {
-    interface IInfo
-    {
-        string GetName();
-        string GetAge();
-    }
-
-    class CA : IInfo
-    {
-        public string Name;
-        public int Age;
-        public string GetAge() { return Age.ToString(); }
-        public string GetName() { return Name; }
-    }
-
-    class CB : IInfo
-    {
-        public string First;
-        public string Last;
-        public double PersonsAge;
-        public string GetName() { return First + " " + Last; }
-        public string GetAge() { return PersonsAge.ToString(); }
-    }
-
-
     class Program
     {
 
-        static void PrintInfo(IInfo item)
-        {
-            Console.WriteLine("Name: {0}, Age {1}", item.GetName(), item.GetAge());
-        }
 
         static void Main()
         {
-            CA a = new CA() { Name = "John Doe", Age = 35 };
-            CB b = new CB() { First = "John", Last = "Doe", PersonsAge = 35 };
-            PrintInfo(a);
-            PrintInfo(b);
+            var myInt = new[] { 20, 4, 16, 9, 2 };  // Create an array of ints.
+
+            Console.WriteLine("Before sort");
+            foreach (var i in myInt)                // Print them out (before sort).
+                Console.Write("{0} ", i);
+            Console.WriteLine(Environment.NewLine + "----------------");
+
+            Array.Sort(myInt);                      // Sort elements by magnitude.
+
+            Console.WriteLine("After sorted");
+            foreach (var i in myInt)                // Print them out (after sorted).
+                Console.Write("{0} ", i);
+            Console.WriteLine(Environment.NewLine + "----------------");
         }
     }
 }
